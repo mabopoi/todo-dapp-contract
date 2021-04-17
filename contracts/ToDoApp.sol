95e330e1980e5703d45452b83ed8fbaf6966df42
unknown <mabopoi@gmail.com>
2021-04-17 00:16:25 -0300

    Writing first functions

--- contracts/ToDoApp.sol
+++ contracts/ToDoApp.sol
@@ -1,4 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract ToDoApp {}
+contract ToDoApp {
+  uint256 public counter = 0;
+
+  struct Task {
+    address author;
+    string title;
+    string description;
+    bool activate;
+  }
+
+  Task[] public tasks;
+
+  mapping(uint256 => Task) public idToTask;
+  mapping(address => uint256) taskAmount;
+
+  function createTask(string calldata _title, string calldata _description)
+    external
+  {
+    Task memory newTask = Task(msg.sender, _title, _description, true);
+    tasks.push(newTask);
+    idToTask[counter] = newTask;
+    taskAmount[msg.sender]++;
+    counter++;
+  }
+
+  function getMyTasks() external view returns (Task[] memory) {
+    Task[] memory result = new Task[](taskAmount[msg.sender]);
+
+    for (uint256 i = 0; i < tasks.length; i++) {
+      uint256 index = 0;
+      if (idToTask[i].author == msg.sender) {
+        result[index] = idToTask[i];
+        index++;
+      }
+    }
+
+    return result;
+  }
+}
